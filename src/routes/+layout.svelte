7bb6ab091c945420a474c5130f354d87e37a4082
amritbrhat <i.amrit.p@gmail.com>
2023-05-08 12:40:03 +0530

    toast

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,18 +3,16 @@
 	import { page } from '$app/stores'
 	import { onMount } from 'svelte'
 	import { browser } from '$app/environment'
+	import { clickOutsideAction } from "svelte-legos"
 	import { goto } from '$app/navigation'
 	import visibilityMode from '$lib/stores/visibility'
 	import WindowBinder from '$lib/components/WindowBinder.svelte'
 	import { innerWidth, outerHeight, scrollY } from '$lib/stores/windows'
 	import siteTour from '$lib/stores/sitetour'
-	import { scale } from 'svelte/transition'
-	import { expoOut } from 'svelte/easing'
 	import Lenis from '@studio-freight/lenis'
 	import '$lib/styles/types.sass'
 	import '$lib/styles/tokens.sass'
 	import Footer from '$lib/components/Footer.svelte'
-	import AllBrands from '$lib/components/AllBrands.svelte'
 	import { lenisStore as lenis, setLenisStore } from '$lib/stores/lenis'
 	import { useScroll } from '$lib/utils/lenisscroll'
 	import { useFrame } from '$lib/utils/lenisframe'
@@ -30,6 +28,11 @@
 	let currentPage = 1
 	const totalPages = 7
 	let tourPage = Array(7).fill(false)
+	let isMessageOpen = true
+
+	function toggleMessage(){
+		isMessageOpen = !isMessageOpen
+	}
 
 	function toggleVisibility() {
 	  if (browser) {
@@ -110,58 +113,84 @@
 <svelte:window bind:innerWidth={innerW}/>
 
 <svelte:head>
-<link rel="preconnect" href="https://fonts.googleapis.com">
-<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous">
-<link href="https://fonts.googleapis.com/css2?family=Encode+Sans:wght@100;200;300;400;500;600;700;800;900&family=Spline+Sans:wght@300;400;500;600;700&display=swap" rel="stylesheet">
 	<link href="https://cdn.jsdelivr.net/npm/textify.js/dist/Textify.min.css" rel="stylesheet"/>
 </svelte:head>
 
 <div id="appbox" class="themer" class:light={$visibilityMode} class:dark={!$visibilityMode}>
 	<WindowBinder/>
 	<slot></slot>
+	{#if isMessageOpen}
+	<div class="globaltoast rta-column">
+		<div class="rta-row">
+			<button class="blank-button" on:click={toggleMessage}>
+			<svg class="closeguy point" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+				<path d="M12 24C5.3724 24 0 18.6276 0 12C0 5.3724 5.3724 0 12 0C18.6276 0 24 5.3724 24 12C24 18.6276 18.6276 24 12 24ZM12 10.3032L8.6064 6.9084L6.9084 8.6064L10.3032 12L6.9084 15.3936L8.6064 17.0916L12 13.6968L15.3936 17.0916L17.0916 15.3936L13.6968 12L17.0916 8.6064L15.3936 6.9084L12 10.3032Z" fill="white"/>
+			</svg>
+			</button>
+		</div>
+		<p>
+			Namaste,
+		</p>
+		<p>
+			Welcome to Bṛhat. We are currently transferring our site hosting server and moving to a new framework.
+		</p>
+		<p>
+			You may experience some bugs, dead-links or accessibility bugs over the next 24-odd hours. Please forgive us for the inconvenience. All systems should be normal very soon!
+		</p>
+		<p>
+			-8 May 2023 1200 hours.
+		</p>
+	</div>
+	{/if}
 	{#if showFooter}
 	<Footer></Footer>
 	{/if}
 </div>
 
-
-
-
-
-
-
 <style lang="sass">
 
-.toast
-	border-radius: 4px
+.closeguy
+	object-fit: contain
+	width: 20px
+	border-radius: 12px
+	height: 20px
+	&:hover
+		path
+			fill: #fe4a49
 
-#firstVisitToast
+.globaltoast
+	.rta-row
+		justify-content: flex-end
+
+.globaltoast.rta-column
 	position: fixed
-	background: transparent
-	border-radius: 4px
-	z-index: 900
-	h4
-		font-weight: 600
-	h6
-		font-weight: 400
+	z-index: 1000
+	background-color: hsla(0,0%,5%,1)
+	background-image: radial-gradient(at 64% 81%, hsla(99,100%,13%,1) 0px, transparent 50%), radial-gradient(at 70% 16%, hsla(0,64%,25%,1) 0px, transparent 50%), radial-gradient(at 0% 0%, hsla(38,85%,39%,1) 0px, transparent 50%)
+	p
+		color: white
+		line-height: 1.2
+		margin-bottom: 12px
 	@media screen and (min-width: 1024px)
-		width: 320px
-		height: 160px
-		bottom: 48px
-		right: 24px
-		h4
-			font-size: 16px
-		h6
+		padding: 16px
+		border-radius: 8px
+		box-shadow: 4px 6px 7px #d7d7d7
+		bottom: 32px
+		right: 32px
+		width: 360px
+		height: 240px
+		p
 			font-size: 14px
 	@media screen and (max-width: 1023px)
-		width: calc(100vw - 64px)
-		height: 160px
-		top: 72px
-		right: 16px
-		h4
-			font-size: 18px
-		h6
-			font-size: 16px
+		padding: 8px
+		box-shadow: none
+		bottom: 0px
+		right: 0px
+	
+		width: 100%
+		height: max-content
+		p
+			font-size: 12px
 
 #appbox
 	width: 100vw
@@ -172,13 +201,6 @@
 	height: 100%
 	display: flex
 	flex-direction: column
-
-.modalclass.light
-	background: rgba(255,255,255,0.6)
-
-.modalclass.dark
-	background: rgba(0,0,0,0.6)
-
 
 </style>
 
