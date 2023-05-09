b2824f6b291f28c1c777ff4ea5d428f0eb8577b0
amritbrhat <i.amrit.p@gmail.com>
2023-05-09 14:56:39 +0530

    removemessage

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -15,17 +15,11 @@
 	let showFooter = true
 	let link:any
 	let firstVisit = false
-	let isMessageOpen = true
 	let fake = false
 
 	function fauxfake(){
 		fake = !fake
 	}
-
-	function toggleMessage(){
-		isMessageOpen = !isMessageOpen
-	}
-
 
 	$: if ( innerW <= 1023 ) {
 		breakPointOn = true
@@ -76,84 +70,12 @@
 <div id="appbox" class="themer" class:light={$visibilityMode} class:dark={!$visibilityMode}>
 
 	<slot></slot>
-	{#if isMessageOpen}
-	<div class="globaltoast rta-column" class:light={$visibilityMode} class:dark={!$visibilityMode}>
-		<div class="rta-row">
-			<div class="blank-button" on:click={toggleMessage} on:keydown={fauxfake}>
-				<svg class="closeguy point" width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-				<path d="M12 24C5.3724 24 0 18.6276 0 12C0 5.3724 5.3724 0 12 0C18.6276 0 24 5.3724 24 12C24 18.6276 18.6276 24 12 24ZM12 10.3032L8.6064 6.9084L6.9084 8.6064L10.3032 12L6.9084 15.3936L8.6064 17.0916L12 13.6968L15.3936 17.0916L17.0916 15.3936L13.6968 12L17.0916 8.6064L15.3936 6.9084L12 10.3032Z" fill="white"/>
-				</svg>
-			</div>
-		</div>
-		<p>
-			Namaste,
-		</p>
-		<p>
-			Welcome to Bṛhat. We are currently transferring our site hosting server and moving to a new framework.
-		</p>
-		<p>
-			You may experience some bugs, dead-links or accessibility bugs over the next 24-odd hours. Please forgive us for the inconvenience. All systems should be normal very soon!
-		</p>
-		<p>
-			-8 May 2023 1200 hours.
-		</p>
-	</div>
-	{/if}
 	{#if showFooter}
 	<Footer></Footer>
 	{/if}
 </div>
 
 <style lang="sass">
-
-.closeguy
-	object-fit: contain
-	width: 20px
-	border-radius: 12px
-	height: 20px
-	&:hover
-		path
-			fill: #fe4a49
-
-.globaltoast
-	.rta-row
-		justify-content: flex-end
-
-.globaltoast.rta-column
-	position: fixed
-	z-index: 1000
-	background-color: hsla(0,0%,5%,1)
-	background-image: radial-gradient(at 64% 81%, hsla(99,100%,13%,1) 0px, transparent 50%), radial-gradient(at 70% 16%, hsla(0,64%,25%,1) 0px, transparent 50%), radial-gradient(at 0% 0%, hsla(38,85%,39%,1) 0px, transparent 50%)
-	p
-		color: white
-		line-height: 1.2
-		margin-bottom: 12px
-	@media screen and (min-width: 1024px)
-		padding: 16px
-		border-radius: 8px
-		box-shadow: 4px 6px 7px #d7d7d7
-		bottom: 32px
-		right: 32px
-		width: 360px
-		height: 240px
-		p
-			font-size: 14px
-	@media screen and (max-width: 1023px)
-		padding: 8px
-		box-shadow: none
-		bottom: 0px
-		right: 0px
-	
-		width: 100%
-		height: max-content
-		p
-			font-size: 12px
-
-.globaltoast.rta-column.light
-	box-shadow: 4px 6px 7px #d7d7d7
-
-.globaltoast.rta-column.dark
-	box-shadow: none
 
 #appbox
 	width: 100vw
