956953bdbb164dfb674d3457dbb098b1db4a21e8
amritbrhat <i.amrit.p@gmail.com>
2023-06-12 14:11:45 +0530

    linkshorten

--- src/lib/links/AnveshiLinks.svelte
+++ src/lib/links/AnveshiLinks.svelte
@@ -15,31 +15,31 @@
 		class="insidelink"
 		in:fly={{ duration: 150, delay: 30, x: 128, y: 0, easing: quintIn }}
 		out:fly={{ duration: 100, delay: 0, x: 128, y: 0, easing: quintIn }}
-		href="/anveshi/chapter/karnataka">Karnataka</a
+		href="/anveshi/karnataka">Karnataka</a
 	>
 	<a
 		class="insidelink"
 		in:fly={{ duration: 150, delay: 60, x: 128, y: 0, easing: quintIn }}
 		out:fly={{ duration: 100, delay: 0, x: 128, y: 0, easing: quintIn }}
-		href="/anveshi/chapter/odisha">Odisha</a
+		href="/anveshi/odisha">Odisha</a
 	>
 	<a
 		class="insidelink"
 		in:fly={{ duration: 150, delay: 90, x: 128, y: 0, easing: quintIn }}
 		out:fly={{ duration: 100, delay: 0, x: 128, y: 0, easing: quintIn }}
-		href="/anveshi/chapter/chamba">Chamba</a
+		href="/anveshi/chamba">Chamba</a
 	>
 	<a
 		class="insidelink"
 		in:fly={{ duration: 150, delay: 90, x: 128, y: 0, easing: quintIn }}
 		out:fly={{ duration: 100, delay: 0, x: 128, y: 0, easing: quintIn }}
-		href="/anveshi/chapter/goa">Goa</a
+		href="/anveshi/goa">Goa</a
 	>
 	<a
 		class="insidelink"
 		in:fly={{ duration: 150, delay: 90, x: 128, y: 0, easing: quintIn }}
 		out:fly={{ duration: 100, delay: 0, x: 128, y: 0, easing: quintIn }}
-		href="/anveshi/chapter/gwalior">Gwalior</a
+		href="/anveshi/gwalior">Gwalior</a
 	>
 	<a
 		class="insidelink"
