4185a9b2c772eb4bdeea07e764d64a41b15da732
amritbrhat <i.amrit.p@gmail.com>
2023-06-28 04:28:52 +0530

    mf30

--- src/routes/svarnanjali/+page.svelte
+++ src/routes/svarnanjali/+page.svelte
@@ -33,8 +33,7 @@
 	$metaDescription =
 		'Svarṇāñjali is a weekly video series to discuss literature, arts, drama, architecture, sculpture, cinema and other fine arts from the point of view of rasikā, in Sanskṛta Niṣṭha Hindī.';
 	$metaUrl = '/svarnanjali';
-	$metaImage =
-		'https://rnfvzaelmwbbvfbsppir.supabase.co/storage/v1/object/public/brhatwebsite/16svarnanjali/krishnaradha.webp';
+	$metaImage = '/images/keys/ki-sv.png';
 	$metaType = 'webpage';
 
 	$: isValidEmail = Boolean(email && email.includes('@'));
