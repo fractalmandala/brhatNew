36f8fbcc2cf05abb735e1f965999404041899dba
amritbrhat <i.amrit.p@gmail.com>
2023-08-03 17:08:42 +0530

    soametaadded

--- src/routes/aryavarta/places/+page.svelte
+++ src/routes/aryavarta/places/+page.svelte
@@ -1,13 +1,28 @@
 <script lang="ts">
 	import { onMount } from 'svelte';
 	import { soaJanapadas } from '$lib/utils/supapulls';
+	import Head from '$lib/components/HeadComponent.svelte';
+	import { metaTitle, metaDescription, metaUrl, metaImage } from '$lib/stores/metastores';
 
 	let places: any;
+	$metaTitle = 'Nagara, Nadi and Janapadas at Scrolls of Āryavarta';
+	$metaDescription =
+		'Travel through time and space to the great lands and cities of ancient Āryavarta.';
+	$metaUrl = 'https://www.brhat.in/aryavarta/places';
+	$metaImage =
+		'https://wganhlzrylmkvvaoalco.supabase.co/storage/v1/object/public/images/janapadas/00.webp';
 
 	onMount(async () => {
 		places = await soaJanapadas();
 	});
 </script>
+
+<Head
+	title={$metaTitle}
+	metaDescription={$metaDescription}
+	metaUrl={$metaUrl}
+	metaImage={$metaImage}
+/>
 
 <div class="rta-column">
 	{#if places && places.length > 0}
